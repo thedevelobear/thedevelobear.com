b0dd4347c687f7d57783b0756d51af6f2c91ded7
Jakub Szewczyk <jakub.szewczyk@stermedia.eu>
2018-09-28 20:47:08 +0200

    Add new service worker

--- static/sw.js
+++ static/sw.js
@@ -1,332 +1,96 @@
-const CACHE_VERSION = 1;
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-const BASE_CACHE_FILES = [
-  '/css/casper-two.css',
-  '/css/index.css',
-  '/post/*.md',
-  '/site.webmanifest',
-  '/favicon.ico',
-];
-
-const OFFLINE_CACHE_FILES = [
-  '/css/casper-two.css',
-  '/css/index.css',
-  '/script.js',
-  '/offline/index.html',
-];
-
-const NOT_FOUND_CACHE_FILES = [
-  '/css/casper-two.css',
-  '/css/index.css',
-  '/script.js',
+var dataCacheName = 'weatherData-v1';
+var cacheName = 'weatherPWA-final-1';
+var filesToCache = [
+  '/',
+  '/index.html',
   '/404.html',
+  '/sw.js',
+  '/css/index.css',
+  '/css/casper-two.css',
+  '/img/Bear-avatar.png',
+  '/img/Bearground.png',
+  '/img/favicon.ico',
+  '/img/logo.png',
 ];
 
-const OFFLINE_PAGE = '/offline/index.html';
-const NOT_FOUND_PAGE = '/404.html';
-
-const CACHE_VERSIONS = {
-  assets: 'assets-v' + CACHE_VERSION,
-  content: 'content-v' + CACHE_VERSION,
-  offline: 'offline-v' + CACHE_VERSION,
-  notFound: '404-v' + CACHE_VERSION,
-};
-
-// Define MAX_TTL's in SECONDS for specific file extensions
-const MAX_TTL = {
-  '/': 3600,
-  html: 3600,
-  json: 86400,
-  js: 86400,
-  css: 86400,
-};
-
-const CACHE_BLACKLIST = [
-  //(str) => {
-  //    return !str.startsWith('http://localhost') && !str.startsWith('https://gohugohq.com');
-  //},
-];
-
-const SUPPORTED_METHODS = [
-  'GET',
-];
-
-/**
- * isBlackListed
- * @param {string} url
- * @returns {boolean}
- */
-function isBlacklisted(url) {
-  return (CACHE_BLACKLIST.length > 0) ? !CACHE_BLACKLIST.filter((rule) => {
-    if(typeof rule === 'function') {
-    return !rule(url);
-  } else {
-    return false;
-  }
-}).length : false
-}
-
-/**
- * getFileExtension
- * @param {string} url
- * @returns {string}
- */
-function getFileExtension(url) {
-  let extension = url.split('.').reverse()[0].split('?')[0];
-  return (extension.endsWith('/')) ? '/' : extension;
-}
-
-/**
- * getTTL
- * @param {string} url
- */
-function getTTL(url) {
-  if (typeof url === 'string') {
-    let extension = getFileExtension(url);
-    if (typeof MAX_TTL[extension] === 'number') {
-      return MAX_TTL[extension];
-    } else {
-      return null;
-    }
-  } else {
-    return null;
-  }
-}
-
-/**
- * installServiceWorker
- * @returns {Promise}
- */
-function installServiceWorker() {
-  return Promise.all(
-    [
-      caches.open(CACHE_VERSIONS.assets)
-        .then(
-          (cache) => {
-        return cache.addAll(BASE_CACHE_FILES);
-}
-),
-  caches.open(CACHE_VERSIONS.offline)
-    .then(
-      (cache) => {
-    return cache.addAll(OFFLINE_CACHE_FILES);
-}
-),
-  caches.open(CACHE_VERSIONS.notFound)
-    .then(
-      (cache) => {
-    return cache.addAll(NOT_FOUND_CACHE_FILES);
-}
-)
-]
-);
-}
-
-/**
- * cleanupLegacyCache
- * @returns {Promise}
- */
-function cleanupLegacyCache() {
-
-  let currentCaches = Object.keys(CACHE_VERSIONS)
-    .map(
-      (key) => {
-    return CACHE_VERSIONS[key];
-}
-);
-
-  return new Promise(
-    (resolve, reject) => {
-
-    caches.keys()
-    .then(
-      (keys) => {
-    return legacyKeys = keys.filter(
-      (key) => {
-      return !~currentCaches.indexOf(key);
-}
-);
-}
-)
-.then(
-    (legacy) => {
-    if (legacy.length) {
-    Promise.all(
-      legacy.map(
-        (legacyKey) => {
-        return caches.delete(legacyKey)
-      }
-    )
-  )
-  .then(
-      () => {
-      resolve()
-    }
-  )
-  .catch(
-      (err) => {
-      reject(err);
-  }
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
   );
-  } else {
-    resolve();
-  }
-}
-)
-.catch(
-    () => {
-    reject();
-}
-);
+});
 
-}
-);
-}
-
-
-self.addEventListener(
-  'install', event => {
-  event.waitUntil(installServiceWorker());
-}
-);
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener(
-  'activate', event => {
-  event.waitUntil(
-  Promise.all(
-    [
-      cleanupLegacyCache(),
-    ]
-  )
-    .catch(
-      (err) => {
-    event.skipWaiting();
-}
-)
-);
-}
-);
-
-self.addEventListener(
-  'fetch', event => {
-
-  event.respondWith(
-  caches.open(CACHE_VERSIONS.content)
-    .then(
-      (cache) => {
-
-    return cache.match(event.request)
-      .then(
-        (response) => {
-
-      if (response) {
-
-        let headers = response.headers.entries();
-        let date = null;
-
-        for (let pair of headers) {
-          if (pair[0] === 'date') {
-            date = new Date(pair[1]);
-          }
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
         }
+      }));
+    })
+  );
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+  return self.clients.claim();
+});
 
-        if (date) {
-          let age = parseInt((new Date().getTime() - date.getTime()) / 1000);
-          let ttl = getTTL(event.request.url);
-
-          if (ttl &amp;&amp; age > ttl) {
-
-            return new Promise(
-              (resolve) => {
-
-              return fetch(event.request)
-                .then(
-                  (updatedResponse) => {
-                if (updatedResponse) {
-                  cache.put(event.request, updatedResponse.clone());
-                  resolve(updatedResponse);
-                } else {
-                  resolve(response)
-                }
-              }
-          )
-          .catch(
-              () => {
-              resolve(response);
-          }
-          );
-
-          }
-          )
-          .catch(
-              (err) => {
-              return response;
-          }
-          );
-          } else {
-            return response;
-          }
-
-        } else {
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
           return response;
-        }
-
-      } else {
-        return null;
-}
-}
-)
-.then(
-  (response) => {
-  if (response) {
-    return response;
+        });
+      })
+    );
   } else {
-    return fetch(event.request)
-      .then(
-        (response) => {
-
-      if(response.status < 400) {
-  if (~SUPPORTED_METHODS.indexOf(event.request.method) &amp;&amp; !isBlacklisted(event.request.url)) {
-    cache.put(event.request, response.clone());
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
   }
-  return response;
-} else {
-  return caches.open(CACHE_VERSIONS.notFound).then((cache) => {
-    return cache.match(NOT_FOUND_PAGE);
-})
-}
-}
-)
-.then((response) => {
-  if(response) {
-    return response;
-  }
-})
-.catch(
-  () => {
-
-  return caches.open(CACHE_VERSIONS.offline)
-    .then(
-      (offlineCache) => {
-    return offlineCache.match(OFFLINE_PAGE)
-  }
-)
-
-}
-);
-}
-}
-)
-.catch(
-  (error) => {
-  console.error('  Error in fetch handler:', error);
-throw error;
-}
-);
-}
-)
-);
-
-}
-);
+});
